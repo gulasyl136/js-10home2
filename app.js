4c73fa7c95416e3585fe2c8dd1cc87f398e5b27b
gulasyl136 <muhtarovagulasyl@gmail.com>
2024-10-30 09:58:23 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// const title = window.document.getElementsByTagName("h1")[0];
+// const title2 = document.getElementsByClassName("class")[0];
+// const title3 = document.getElementById("id");
+// const descripion = document.querySelector("p");
+// const descripion2 = document.querySelector(".lorem");
+// const descripion3 = document.querySelector("#dis");
+// const uni = document.querySelectorAll("p");
+
+// // озгортуу учун
+// title.textContent = "Nursultan";
+// console.log(title);
+// title2.textContent = "karta";
+// console.log(title2);
+// title3.textContent = "card"
+// console.log(title3);
+// descripion.textContent = "today is sunny"
+// console.log(descripion);
+// descripion2.textContent = "I like this weather"
+// console.log(descripion2);
+// descripion3.textContent = "i am at a classroom"
+// console.log(descripion3);
+
+
+
+// title.style.backgroundColor = 'red'
+// title.style.color = 'red'
+// at event listener
+
+
